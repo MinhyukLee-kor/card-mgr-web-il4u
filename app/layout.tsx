7e40195419d5f6117ec3279a0fd4d96343d1361e
minhyuk <kazesky93@gmail.com>
2025-01-18 18:15:20 +0900

    ios 사파리 화면 확대 오류 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,12 @@ export default function RootLayout({
 
   return (
     <html lang="ko">
+      <head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           {showHeader && <Header />}
